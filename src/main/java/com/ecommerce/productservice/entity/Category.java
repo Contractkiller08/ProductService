de4547712bd2bef6249efb4ce12de2233291d83e
Contractkiller08 <satyamjha0812@gmail.com>
2024-09-09 14:53:44 +0530

    Implemented update category function

--- src/main/java/com/ecommerce/productservice/entity/Category.java
+++ src/main/java/com/ecommerce/productservice/entity/Category.java
@@ -3,6 +3,7 @@ package com.ecommerce.productservice.entity;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
--- src/main/java/com/ecommerce/productservice/service/CategoryService.java
+++ src/main/java/com/ecommerce/productservice/service/CategoryService.java
@@ -2,8 +2,11 @@ package com.ecommerce.productservice.service;
 
 import com.ecommerce.productservice.dto.CategoryDTO;
 import com.ecommerce.productservice.entity.Category;
+import com.ecommerce.productservice.exception.CategoryNotFoundException;
 import com.ecommerce.productservice.repository.CategoryRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class CategoryService {
@@ -19,4 +22,20 @@ public class CategoryService {
 
         return categoryRepository.save(newcategory);
     }
+
+    //updation of category
+    public Category updateCategory(String categoryId, CategoryDTO categoryDTO){
+        //check if category already exists or not
+        Optional<Category> categId = categoryRepository.findById(categoryId);
+
+        if(categId.isPresent()){
+            Category existingCateg = categId.get();
+            existingCateg.setName(categoryDTO.getName());
+            existingCateg.setDescription(categoryDTO.getDescription());
+            return categoryRepository.save(existingCateg);
+        }else{
+            throw new CategoryNotFoundException("Category with ID " + categoryId + " not found.");
+        }
+    }
+
 }
