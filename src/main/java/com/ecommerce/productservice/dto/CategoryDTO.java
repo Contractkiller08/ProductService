17442324b18ec87fa66b52effc43b93295385e2e
Contractkiller08 <satyamjha0812@gmail.com>
2024-09-10 18:17:51 +0530

    DD connection added and refactore category dto

--- src/main/java/com/ecommerce/productservice/dto/CategoryDTO.java
+++ src/main/java/com/ecommerce/productservice/dto/CategoryDTO.java
@@ -1,5 +1,6 @@
 package com.ecommerce.productservice.dto;
 
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Builder;
 import lombok.Data;
 import lombok.Getter;
@@ -10,6 +11,8 @@ import lombok.Setter;
 @Builder
 public class CategoryDTO {
     private String id;
+    @NotEmpty(message = "name can't be empty")
     private String name;
+    @NotEmpty(message = "description can't be empty")
     private String description;
 }
