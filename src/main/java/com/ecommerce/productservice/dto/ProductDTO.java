f85c2950ccda947af8170ceaa7017c57c8aac1dc
Contractkiller08 <satyamjha0812@gmail.com>
2024-09-10 18:25:55 +0530

    testing

--- src/main/java/com/ecommerce/productservice/dto/ProductDTO.java
+++ src/main/java/com/ecommerce/productservice/dto/ProductDTO.java
@@ -1,11 +1,13 @@
 package com.ecommerce.productservice.dto;
 
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Data;
 
 @Data
 public class ProductDTO{
 
     private String id;
+    @NotEmpty(message = "name can't be empty")
     private String name;
     private int availableCount;
     private double price;
